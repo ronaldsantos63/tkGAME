73beea8952bd356a8066b9ee2be01c8cc45a0d07
Raphaël SEBAN <motus@laposte.net>
2014-03-06 18:02:52 +0100

    Update tkgame.py

--- tkgame.py
+++ tkgame.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+tkGAME - all-in-one Game library for Tkinter
+Copyright (c) 2014+ Raphaël Seban <motus@laposte.net>
+
+"""
+
+# lib imports
+import tkRAD
+
+class Application (tkRAD.RADApplication):
+
+    pass
+
+# end class Application
+
+# launch app
+if __name__ == "__main__":
+
+    Application().run()
+
+# end if
