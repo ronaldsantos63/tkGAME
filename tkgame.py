7cd6f04ff31c7cc04c866e470ac6707d6b27350f
Raphaël SEBAN <motus@laposte.net>
2014-03-06 18:05:57 +0100

    Update tkgame.py

--- tkgame.py
+++ tkgame.py
@@ -5,6 +5,11 @@
 tkGAME - all-in-one Game library for Tkinter
 Copyright (c) 2014+ Raphaël Seban <motus@laposte.net>
 
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
 # lib imports
