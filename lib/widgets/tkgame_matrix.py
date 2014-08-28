220c0bbe17c1a9d4d41f2fa7bbdc67cb656f5e71
Raphaël SEBAN <motus@laposte.net>
2014-08-28 09:40:28 +0200

    updated code in lib

--- lib/widgets/tkgame_matrix.py
+++ lib/widgets/tkgame_matrix.py
@@ -34,69 +34,90 @@ class TkGameMatrix:
     BOTTOM_LEFT = "bottom left"
     BOTTOM_RIGHT = "bottom right"
 
+    CELLSIZE = 64
 
-    def __init__ (self, matrix_data, matrix_defs, cellsize=0):
+
+    def __init__ (self, **kw):
         """
             class constructor
         """
-        self.data = matrix_data
-        self.defs = matrix_defs
-        self.cellsize = cellsize
+        self.__internal_data = dict()
+        self.rows = kw.get("rows") or 0
+        self.columns = kw.get("columns") or 0
+        self.cellsize = kw.get("cellsize") or self.CELLSIZE
+        # external matrix data support
+        self.resize(kw.get("data"))
+    # end def
+
+
+    def _rebind (self, xy, bbox, circular_xy=None):
+        """
+            protected method - generic rebinding implementation;
+        """
+        # inits
+        x, y = xy
+        xmin, ymin, xmax, ymax = bbox
+        # circular rebindings
+        if circular_xy:
+            circular_x, circular_y = circular_xy
+            if circular_x:
+                if x < xmin:
+                    x = max(xmin, xmax)
+                elif x > xmax:
+                    x = xmin
+                # end if
+            # end if
+            if circular_y:
+                if y < ymin:
+                    y = max(ymin, ymax)
+                elif y > ymax:
+                    y = ymin
+                # end if
+            # end if
+        # classical rebindings
+        else:
+            x = max(xmin, min(xmax, x))
+            y = max(ymin, min(ymax, y))
+        # end if
+        # return results
+        return (x, y)
+    # end def
+
+
+    def at (self, row_column):
+        """
+            retrieves object at row_column = (row, column), if exists;
+        """
+        return self.internal_data.get(row_column)
+    # end def
+
+
+    def at_xy (self, xy):
+        """
+            retrieves object at (x, y) converted to matrix location
+            (row, column) or None if object is not found;
+        """
+        return self.at(self.row_column(xy))
     # end def
 
 
     def bbox (self):
         """
-            returns estimated graphical bounding box of the matrix
+            returns matrix bounding box;
         """
-        return (0, 0, self.columns * self.cellsize, self.rows * self.cellsize)
+        return (0, 0, max(0, self.rows - 1), max(0, self.columns - 1))
     # end def
 
 
-    def center_xy (self, row, column):
+    def bbox_xy (self):
         """
-            returns center (x, y) coordinates of a matrix cell
-            located at (row, column);
+            returns estimated graphical bounding box of the matrix;
         """
         return (
-            (column + 0.5) * self.cellsize, (row + 0.5) * self.cellsize
+            0, 0,
+            max(0, self.columns * self.cellsize - 1),
+            max(0, self.rows * self.cellsize - 1)
         )
-    # end def
-
-
-    def corner_xy (self, row, column, corner=None):
-        """
-            returns top/bottom left/right (x, y) corner coordinates
-            of a matrix cell located at (row, column); parameter
-            @corner should be one of self.TOP_LEFT, self.TOP_RIGHT,
-            self.BOTTOM_LEFT or self.BOTTOM_RIGHT; will default to
-            self.TOP_LEFT if omitted or incorrect param value;
-        """
-        dx, dy = {
-            self.TOP_LEFT: (0, 0),
-            self.TOP_RIGHT: (1, 0),
-            self.BOTTOM_LEFT: (0, 1),
-            self.BOTTOM_RIGHT: (1, 1),
-        }.get(corner) or (0, 0)
-        return (
-            (column + dx) * self.cellsize, (row + dy) * self.cellsize
-        )
-    # end def
-
-
-    def row_column (self, x, y):
-        """
-            converts an (x, y) position to (row, column) position
-        """
-        return (x//self.cellsize, y//self.cellsize)
-    # end def
-
-
-    def width_height (self):
-        """
-            returns estimated graphical (width, height) of the matrix
-        """
-        return (self.columns * self.cellsize, self.rows * self.cellsize)
     # end def
 
 
@@ -110,7 +131,7 @@ class TkGameMatrix:
 
     @cellsize.setter
     def cellsize (self, value):
-        self.__cellsize = abs(int(value))
+        self.__cellsize = max(0, int(value))
     # end def
 
     @cellsize.deleter
@@ -119,30 +140,384 @@ class TkGameMatrix:
     # end def
 
 
-    @property
-    def data (self):
+    def center_xy (self, row_column):
         """
-            matrix data
+            returns center (x, y) coordinates of a matrix cell
+            located at (row, column);
         """
-        return self.__data
+        row, column = row_column
+        return (
+            (column + 0.5) * self.cellsize, (row + 0.5) * self.cellsize
+        )
     # end def
 
-    @data.setter
-    def data (self, value):
-        if isinstance(value, list):
-            self.__data = value
-            self.rows = len(value)
-            self.columns = max(0, 0, *map(len, value))
-        else:
-            raise TypeError(
-                "matrix data must be of type 'list'."
+
+    def coords (self):
+        """
+            returns list of available (row, column) coordinate tuples;
+        """
+        return self.internal_data.keys()
+    # end def
+
+
+    def corner_xy (self, row_column, corner=None):
+        """
+            returns top/bottom left/right (x, y) corner coordinates
+            of a matrix cell located at (row, column);
+            parameter @corner should be one of self.TOP_LEFT,
+            self.TOP_RIGHT, self.BOTTOM_LEFT or self.BOTTOM_RIGHT;
+            will default to self.TOP_LEFT if omitted or incorrect
+            param value;
+        """
+        # inits
+        row, column = row_column
+        # relative_row, relative_column
+        rr, rc = {
+            self.TOP_LEFT: (0, 0),
+            self.TOP_RIGHT: (0, 1),
+            self.BOTTOM_LEFT: (1, 0),
+            self.BOTTOM_RIGHT: (1, 1),
+        }.get(corner) or (0, 0)
+        return ((column + rc) * self.cellsize, (row + rr) * self.cellsize)
+    # end def
+
+
+    @property
+    def internal_data (self):
+        """
+            matrix internal data (READ-ONLY property);
+        """
+        return self.__internal_data
+    # end def
+
+    @internal_data.setter
+    def internal_data (self, value):
+        """
+            forbidden - READ-ONLY internal data
+        """
+        raise TkGameMatrixError(
+            "'internal_data' attribute is READ-ONLY."
+        )
+    # end def
+
+    @internal_data.deleter
+    def internal_data (self):
+        del self.__internal_data
+    # end def
+
+
+    def drop (self, row_column, raise_error=False):
+        """
+            deletes object located at (row, column);
+            if @raise_error is True and no object found, raises
+            TkGameMatrixCellError;
+        """
+        # inits
+        _object = self.at(row_column)
+        # got something to delete?
+        if _object:
+            # silent drops...
+            self.internal_data.pop(row_column, None)
+        # error handling
+        elif raise_error:
+            # raise error
+            raise TkGameMatrixCellError(
+                "while trying to delete: "
+                "no object found in matrix cell."
             )
         # end if
     # end def
 
-    @data.deleter
-    def data (self):
-        del self.__data
+
+    def drop_xy (self, xy, raise_error=False):
+        """
+            deletes object located at (x, y) adjusted to matrix
+            (row, column) location;
+            if @raise_error is True and no object found, raises
+            TkGameMatrixCellError;
+        """
+        self.drop(self.row_column(xy), raise_error)
+    # end def
+
+
+    def duplicate (self, from_rowcol, to_rowcol, raise_error=False):
+        """
+            duplicates object located at from_rowcol into to_rowcol
+            location;
+            if @raise_error is True:
+            - raises TkGameMatrixCellError if destination is not None,
+            - raises TkGameMatrixCellError if source is None;
+        """
+        self.move(from_rowcol, to_rowcol, raise_error, duplicate=True)
+    # end def
+
+
+    def duplicate_xy (self, from_xy, to_xy, raise_error=False):
+        """
+            duplicates object located at from_xy into to_xy all
+            adjusted to matrix (row, column) locations;
+            if @raise_error is True:
+            - raises TkGameMatrixCellError if destination is not None,
+            - raises TkGameMatrixCellError if source is None;
+        """
+        self.move_xy(from_xy, to_xy, raise_error, duplicate=True)
+    # end def
+
+
+    def move (self, from_rowcol, to_rowcol,
+                                    raise_error=False, duplicate=False):
+        """
+            absolute move from (row0, column0) to (row1, column1);
+            if @raise_error is True:
+            - raises TkGameMatrixCellError if destination is not None,
+            - raises TkGameMatrixCellError if source is None;
+        """
+        # look for destination object
+        _object = self.at(to_rowcol)
+        # error handling
+        if _object and raise_error:
+            raise TkGameMatrixCellError(
+                "while trying to move/duplicate: "
+                "destination cell is busy."
+            )
+        # it's OK, let's try to move/duplicate
+        else:
+            # look for source object
+            _object = self.at(from_rowcol)
+            # got something?
+            if _object:
+                # move it!
+                self.set_at(to_rowcol, _object)
+                # no duplication (simple move)?
+                if not duplicate:
+                    # remove from source location
+                    self.internal_data.pop(from_rowcol, None)
+                # end if
+            # no source object found
+            elif raise_error:
+                # error handling
+                raise TkGameMatrixCellError(
+                    "while trying to move/duplicate: "
+                    "no object found in source cell."
+                )
+            # end if
+        # end if
+    # end def
+
+
+    def move_xy (self, from_xy, to_xy,
+                                    raise_error=False, duplicate=False):
+        """
+            absolute move from (x0, y0) to (x1, y1) all adjusted to
+            matrix (row, column) locations;
+            if @raise_error is True:
+            - raises TkGameMatrixCellError if destination is not None,
+            - raises TkGameMatrixCellError if source is None;
+        """
+        self.move(
+            self.row_column(from_xy),
+            self.row_column(to_xy),
+            raise_error,
+            duplicate
+        )
+    # end def
+
+
+    def objects (self):
+        """
+            returns list of matrix' registered objects;
+        """
+        return self.internal_data.values()
+    # end def
+
+
+    def rebind (self, row_column, circular=False):
+        """
+            rebinds (row, column) matrix location to fit into
+            current matrix bounding box (bbox);
+            if @circular is True, any overflow will return to zero;
+            returns new rebound (row, column) coordinates;
+        """
+        # return results
+        return self._rebind(row_column, self.bbox(), circular)
+    # end def
+
+
+    def rebind_xy (self, xy, circular=False):
+        """
+            rebinds (x, y) coordinates to fit into current matrix
+            bounding box (bbox_xy);
+            if @circular is True, any overflow will return to zero;
+            returns new rebound (x, y) coordinates;
+        """
+        # return results
+        return self._rebind(xy, self.bbox_xy(), circular)
+    # end def
+
+
+    def rel_at (self, from_rowcol, rel_rowcol):
+        """
+            retrieves object at relative location, if exists;
+        """
+        # inits
+        row, column = from_rowcol
+        # relative_row, relative_column
+        rr, rc = rel_rowcol
+        # get object
+        return self.at((row + rr, column + rc))
+    # end def
+
+
+    def rel_at_xy (self, from_xy, rel_xy):
+        """
+            retrieves object at a relative (x, y) location, if any;
+        """
+        # inits
+        x, y = from_xy
+        rel_x, rel_y = rel_xy
+        return self.at(self.row_column((x + rel_x, y + rel_y)))
+    # end def
+
+
+    def rel_duplicate (self, from_rowcol, rel_rowcol, raise_error=False):
+        """
+            duplicates object located at from_rowcol to relative
+            rel_rowcol location;
+            if @raise_error is True:
+            - raises TkGameMatrixCellError if destination is not None,
+            - raises TkGameMatrixCellError if source is None;
+        """
+        self.rel_move(
+            from_rowcol, rel_rowcol, raise_error, duplicate=True
+        )
+    # end def
+
+
+    def rel_duplicate_xy (self, from_xy, rel_xy, raise_error=False):
+        """
+            duplicates object located at from_xy into relative
+            location rel_xy all adjusted to matrix (row, column)
+            locations;
+            if @raise_error is True:
+            - raises TkGameMatrixCellError if destination is not None,
+            - raises TkGameMatrixCellError if source is None;
+        """
+        self.rel_move_xy(from_xy, rel_xy, raise_error, duplicate=True)
+    # end def
+
+
+    def rel_move (self, from_rowcol, rel_rowcol,
+                                    raise_error=False, duplicate=False):
+        """
+            relative move from (row, column) to (row + rel_row,
+            column + rel_column);
+            if @raise_error is True:
+            - raises TkGameMatrixCellError if destination is not None,
+            - raises TkGameMatrixCellError if source is None;
+        """
+        # inits
+        row, column = from_rowcol
+        rr, rc = rel_rowcol
+        self.move(
+            from_rowcol,
+            (row + rr, column + rc),
+            raise_error,
+            duplicate
+        )
+    # end def
+
+
+    def rel_move_xy (self, from_xy, rel_xy,
+                                    raise_error=False, duplicate=False):
+        """
+            relative move from (x, y) to (x + rel_x, y + rel_y)
+            using rel_xy all adjusted to matrix (row, column)
+            locations;
+            if @raise_error is True:
+            - raises TkGameMatrixCellError if destination is not None,
+            - raises TkGameMatrixCellError if source is None;
+        """
+        # inits
+        x, y = from_xy
+        rx, ry = rel_xy
+        self.move_xy(from_xy, (x + rx, y + ry), raise_error, duplicate)
+    # end def
+
+
+    def resize (self, matrix_data):
+        """
+            resizes inner matrix (rows, columns) along with
+            @matrix_data;
+            this parameter must be at least a list of iterables;
+        """
+        # param controls
+        if matrix_data:
+            # inits
+            self.data = list(matrix_data)
+            self.rows = len(self.data)
+            self.columns = max(0, 0, *map(len, self.data))
+            # reset internal data
+            self.internal_data.clear()
+            # return results
+            return (self.rows, self.columns)
+        # end if
+        # no data, no dims
+        return (0, 0)
+    # end def
+
+
+    def row_column (self, xy):
+        """
+            converts an xy = (x, y) position to (row, column) position
+        """
+        # inits
+        x, y = xy
+        return (y//self.cellsize, x//self.cellsize)
+    # end def
+
+
+    def set_at (self, row_column, object_):
+        """
+            sets object at row_column = (row, column);
+        """
+        self.internal_data[row_column] = object_
+    # end def
+
+
+    def set_at_xy (self, xy, object_):
+        """
+            sets object at xy = (x, y) converted to a common matrix
+            (row, column) location;
+        """
+        self.internal_data[self.row_column(xy)] = object_
+    # end def
+
+
+    def width_height (self):
+        """
+            returns estimated graphical (width, height) of the matrix
+        """
+        return (self.columns * self.cellsize, self.rows * self.cellsize)
     # end def
 
 # end class TkGameMatrix
+
+
+# exception handling
+
+class TkGameMatrixError (Exception):
+    """
+        handles matrix specific errors;
+    """
+    pass
+# end class TkGameMatrixError
+
+
+# exception handling
+
+class TkGameMatrixCellError (Exception):
+    """
+        handles matrix' cell specific errors;
+    """
+    pass
+# end class TkGameMatrixCellError
--- lib/widgets/tkgame_sprite.py
+++ lib/widgets/tkgame_sprite.py
@@ -24,9 +24,12 @@
 
 # lib imports
 import os.path as OP
+
+# mandatory dependencies
 from . import tkgame_events as EM
 from . import tkgame_images as IM
 from . import tkgame_animations as AP
+from . import tkgame_matrix as MX
 
 
 class TkGameSprite:
@@ -44,15 +47,14 @@ class TkGameSprite:
         },
     }
 
-    CELLSIZE = 64
 
-
-    def __init__ (self, owner, canvas, **kw):
+    def __init__ (self, owner, matrix, canvas, **kw):
         """
             class constructor
         """
         # member inits
         self.owner = owner
+        self.matrix = matrix
         self.canvas = canvas
         self.events = EM.get_event_manager()
         self.animations = AP.get_animation_pool()
@@ -61,10 +63,10 @@ class TkGameSprite:
         self.state = kw.get("state") or "default"
         self.canvas_id = kw.get("cid") or 0
         self.canvas_tags = kw.get("tags") or ""
-        self.cellsize = kw.get("cellsize") or self.CELLSIZE
-        self.dict_ids = kw.get("dict_ids") or dict()
-        self.x = kw.get("x") or 0
-        self.y = kw.get("y") or 0
+        self.xy = (kw.get("x"), kw.get("y"))
+        self.row_column = (kw.get("row") or 0, kw.get("column") or 0)
+        # for best simplification - hook method
+        self.init_sprite(**kw)
     # end def
 
 
@@ -82,107 +84,6 @@ class TkGameSprite:
         """
         x0, y0, x1, y1 = self.bbox()
         return ((x0 + x1)/2, (y0 + y1)/2)
-    # end def
-
-
-    def collisions (self, xy=None):
-        """
-            returns list of canvas IDs of sprites colliding with
-            this current sprite; this list excludes current sprite's
-            ID; parameter xy must be an (x, y) tuple i.e. xy=(x, y);
-        """
-        # param inits
-        if xy is None:
-            # sprite inbound collision detection
-            xy = self.bbox()
-        else:
-            # sprite outbound collision detection
-            xy *= 2
-        # end if
-        # get collisions
-        collisions = list(self.canvas.find_overlapping(*xy))
-        # strip out self id
-        if self.canvas_id in collisions:
-            collisions.remove(self.canvas_id)
-        # end if
-        return collisions
-    # end def
-
-
-    def look_ahead (self, sx, sy):
-        """
-            looks around current sprite to see who might collide;
-        """
-        # inits
-        dx = sx * self.cellsize
-        dy = sy * self.cellsize
-        sprite = None
-        # look ahead
-        collisions = self.collisions(xy=(self.x + dx, self.y + dy))
-        if collisions:
-            sprite = self.dict_ids.get(collisions[0])
-        # end if
-        return {"sprite": sprite, "dx": dx, "dy": dy}
-    # end def
-
-
-    def move_sprite (self, sx, sy, callback=None):
-        """
-            moves sprite along callback function's truth return
-            value; if callback is omitted, sprite will move when no
-            other sprite is encountered at destination place;
-            callback function will get in argument
-            self.look_ahead()'s return value;
-        """
-        # param inits
-        if not callable(callback):
-            callback = lambda c: not c["sprite"]
-        # end if
-        # look ahead
-        c_dict = self.look_ahead(sx, sy)
-        # allowed to move?
-        if callback(c_dict):
-            # move sprite
-            self.move_animation(c_dict)
-        # end if
-    # end def
-
-
-    def move_animation (self, c_dict):
-        """
-            here is the animation of a moving sprite
-        """
-        # moving is quite simple here
-        # but you can reimplement this in your own subclasses
-        self.canvas.move(self.canvas_id, c_dict["dx"], c_dict["dy"])
-        # update pos
-        self.x += c_dict["dx"]
-        self.y += c_dict["dy"]
-    # end def
-
-
-    def start (self):
-        """
-            starting sprite's image animation loop; sprite's owner
-            must implement an owner.register_sprite(canvas_id,
-            sprite) method;
-        """
-        # sets up sprite if not already done
-        if not self.canvas_id:
-            self.canvas_id = self.canvas.create_image(
-                self.x, self.y, anchor='center', tags=self.canvas_tags,
-            )
-            # notify sprite creation (e.g. for registering)
-            self.events.raise_event(
-                "Canvas:Sprite:Created",
-                canvas_id=self.canvas_id,
-                sprite=self,
-            )
-            # loading sprite's animation pictures
-            self.load_images()
-        # end if
-        # enter the loop
-        self.animations.run_after(1, self.image_animation_loop)
     # end def
 
 
@@ -226,23 +127,6 @@ class TkGameSprite:
     # end def
 
 
-    def on_sequence_end (self, *args, **kw):
-        """
-            this is called once a status image sequence ends;
-            please, feel free to override this in your own subclasses;
-        """
-        pass
-    # end def
-
-
-    def load_images (self):
-        """
-            cacheing all sprite states pictures
-        """
-        self.image_manager.load_images(self.images_dir)
-    # end def
-
-
     @property
     def images_dir (self):
         """
@@ -262,6 +146,155 @@ class TkGameSprite:
     # end def
 
 
+    def init_sprite (self, **kw):
+        """
+            hook method to be reimplemented in subclass;
+            this avoids re-declaring __init__ signatures all the time;
+        """
+        # put your own code in subclasses
+        pass
+    # end def
+
+
+    def load_images (self):
+        """
+            cacheing all sprite states pictures
+        """
+        self.image_manager.load_images(self.images_dir)
+    # end def
+
+
+    def look_ahead (self, sx, sy):
+        """
+            looks around current sprite to see who might collide;
+        """
+        # inits
+        rel_xy = (sx * self.matrix.cellsize, sy * self.matrix.cellsize)
+        # look ahead
+        sprite = self.matrix.rel_at_xy(self.xy, rel_xy)
+        # return data
+        return {"sprite": sprite, "rel_xy": rel_xy}
+    # end def
+
+
+    @property
+    def matrix (self):
+        """
+            game matrix attribute;
+            must be of TkGameMatrix type;
+        """
+        return self.__matrix
+    # end def
+
+    @matrix.setter
+    def matrix (self, value):
+        """
+            game matrix attribute;
+            must be of TkGameMatrix type;
+        """
+        if isinstance(value, MX.TkGameMatrix):
+            self.__matrix = value
+        else:
+            raise TypeError(
+                "'matrix' attribute must be of "
+                "'TkGameMatrix' type or at least a subclass of this."
+            )
+        # end if
+    # end def
+
+    @matrix.deleter
+    def matrix (self):
+        del self.__matrix
+    # end def
+
+
+    def move_animation (self, c_dict):
+        """
+            here is the animation of a moving sprite
+        """
+        # moving is quite simple here
+        # but you can reimplement this in your own subclasses
+        dx, dy = c_dict["rel_xy"]
+        # relative move on canvas
+        self.canvas.move(self.canvas_id, dx, dy)
+        # update matrix
+        self.matrix.rel_move_xy(self.xy, (dx, dy))
+        # update pos
+        self.x += dx
+        self.y += dy
+    # end def
+
+
+    def move_sprite (self, sx, sy, callback=None):
+        """
+            moves sprite along callback function's truth return
+            value; if callback is omitted, sprite will move when no
+            other sprite is encountered at destination place;
+            callback function will get in argument
+            self.look_ahead()'s return value;
+        """
+        # param inits
+        if not callable(callback):
+            callback = lambda c_dict: not c_dict["sprite"]
+        # end if
+        # look ahead
+        c_dict = self.look_ahead(sx, sy)
+        # allowed to move?
+        if callback(c_dict):
+            # move sprite
+            self.move_animation(c_dict)
+        # end if
+    # end def
+
+
+    def on_sequence_end (self, *args, **kw):
+        """
+            this is called once a status image sequence ends;
+            please, feel free to override this in your own subclasses;
+        """
+        pass
+    # end def
+
+
+    @property
+    def row_column (self):
+        """
+            sprite's (row, column) location on matrix;
+        """
+        return self.matrix.row_column(self.xy)
+    # end def
+
+    @row_column.setter
+    def row_column (self, value):
+        # inits
+        self.xy = self.matrix.center_xy(value)
+    # end def
+
+
+    def start (self):
+        """
+            starting sprite's image animation loop;
+        """
+        # sets up sprite if not already done
+        if not self.canvas_id:
+            # create sprite on canvas
+            self.canvas_id = self.canvas.create_image(
+                self.x, self.y, anchor='center', tags=self.canvas_tags,
+            )
+            # load sprite's animation pictures
+            self.load_images()
+            # notify sprite creation (e.g. for registering)
+            self.events.raise_event(
+                "Canvas:Sprite:Created",
+                canvas_id=self.canvas_id,
+                sprite=self,
+            )
+        # end if
+        # enter the loop
+        self.animations.run_after(1, self.image_animation_loop)
+    # end def
+
+
     @property
     def state (self):
         """
@@ -272,13 +305,39 @@ class TkGameSprite:
 
     @state.setter
     def state (self, value):
-        self.__state = str(value)
-        self.state_counter = 0
+        if value in self.STATUS:
+            self.__state = str(value)
+            self.state_counter = 0
+        else:
+            raise TkGameSpriteError(
+                "unsupported value '{v}' for 'state' attribute."
+                .format(v=value)
+            )
+        # end if
     # end def
 
     @state.deleter
     def state (self):
         del self.__state
+    # end def
+
+
+    @property
+    def x (self):
+        """
+            x coordinate of current sprite
+        """
+        return self.__x
+    # end def
+
+    @x.setter
+    def x (self, value):
+        self.__x = value or 0
+    # end def
+
+    @x.deleter
+    def x (self):
+        del self.__x
     # end def
 
 
@@ -300,4 +359,33 @@ class TkGameSprite:
         del self.x, self.y
     # end def
 
+
+    @property
+    def y (self):
+        """
+            y coordinate of current sprite
+        """
+        return self.__y
+    # end def
+
+    @y.setter
+    def y (self, value):
+        self.__y = value or 0
+    # end def
+
+    @y.deleter
+    def y (self):
+        del self.__y
+    # end def
+
 # end class TkGameSprite
+
+
+# error handling
+
+class TkGameSpriteError (Exception):
+    """
+        handles sprite specific errors;
+    """
+    pass
+# end class TkGameSpriteError
