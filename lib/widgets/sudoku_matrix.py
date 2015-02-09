7bfe84061c9bf75d80178d61d202f15e57f23931
Raphaël SEBAN <motus@laposte.net>
2015-02-09 10:12:14 +0100

    working on sudoku_matrix.py module

--- lib/widgets/sudoku_matrix.py
+++ lib/widgets/sudoku_matrix.py
@@ -272,10 +272,9 @@ class SudokuMatrix (Matrix):
         from 1 to 9, alphabetical letters or even symbols;
         - each CELL may contain 1 to 9 ITEMS or the Python value None;
         - for historical reasons, ITEMS are frequently called VALUES;
-        - a VALUE is always an UNIQUE ITEM into a UNIQUE CELL;
+        - a VALUE is always an UNIQUE ITEM into a distinct CELL;
         - to avoid confusion, we choose here to call VALUES unique ITEM
-        into SEVERAL CELLS and ITEMS several values into an UNIQUE
-        CELL;
+        into SEVERAL CELLS and ITEMS several values into a UNIQUE CELL;
         - each ITEM must appear ONLY ONCE into a given UNIT i.e. only
         once into a given ROW, only once into a given COLUMN and only
         once into a given BOX;
